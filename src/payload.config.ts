71a0f39633c51bb7447ee6b5d79b3d36ef9483f8
leandrosemog <lekocastro@gmail.com>
2026-07-12 02:28:50 -0300

    chore: adota Biome para lint/format (remove ESLint/Prettier)

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,12 +1,11 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
 import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
 import sharp from 'sharp'
-
-import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { Users } from './collections/Users'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
